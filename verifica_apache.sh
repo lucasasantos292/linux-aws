0bbc63edbd8f7bff89ef5e0bac6658754f9b4b75
Lucas <lucasasantos292@github.com>
2023-01-25 11:54:01 -0300

    atualizando caminho arquivos

--- verifica_apache.sh
+++ verifica_apache.sh
@@ -6,13 +6,13 @@ codigo_http=$(curl -s -o /dev/null -I -w "%{http_code}" $ip)
 
 if [ $codigo_http -ne 200 ]; then
 
-    echo "$(date +%F\ %T) Apache está OFFLINE! Código $codigo_http " >> /home/lucas/logs/servidor_off.log
+    echo "$(date +%F\ %T) Apache está OFFLINE! Código $codigo_http " >> /mnt/efs/lucasalves/servidor_off
 
     systemctl restart httpd
 
 else
 
-    echo "$(date +%F\ %T) Apache está ONLINE" >> /home/lucas/logs/servidor_on.log
+    echo "$(date +%F\ %T) Apache está ONLINE" >> /mnt/efs/lucasalves/servidor_on
 
 fi
 
